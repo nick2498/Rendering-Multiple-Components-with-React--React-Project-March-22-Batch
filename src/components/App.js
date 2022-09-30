58b3d5fd2119ec08964773f47990aac7ed186950
Naitik Gupta <guptanaitik98@gmail.com>
2022-09-30 18:55:49 +0530

    update code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">E-commerce Website</h1>
+      <p data-ns-test="project-description">
+        I want to built my own shoping website like Myntra and Ajio.
+      </p>
+    </div>
+  );
+};
 
 export default App;
